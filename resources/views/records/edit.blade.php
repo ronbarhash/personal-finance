718ccbe8a1a941c385c795d0e398fbf7d4b2eaec
Ron Barhash <rzh@ukr.net>
2017-03-12 17:26:17 +0200

    fix

--- resources/views/records/edit.blade.php
+++ resources/views/records/edit.blade.php
@@ -11,13 +11,13 @@
    
     <div class="form-group">
         {{ Form::label('title', 'Название') }}
-        {{ Form::text('title', Input::old('title'), array('class' => 'form-control')) }} 
+        {{ Form::text('title', Input::old('title'), array('class' => 'form-control', 'placeholder'=>'Название')) }} 
 
         {{ Form::label('date_of', 'Дата') }}
         {{ Form::text('date_of', \Carbon\Carbon::now(), array('class' => 'form-control')) }} 
 
         {{ Form::label('cost', 'Сумма') }}
-        {{ Form::text('cost', Input::old('cost'), array('class' => 'form-control')) }}      
+        {{ Form::text('cost', Input::old('cost'), array('class' => 'form-control', 'placeholder'=>'0.00')) }}      
     </div>
     <div class="form-group">
         {{ Form::label('record_type', 'Тип') }}
@@ -25,7 +25,7 @@
     </div>
 
     <div class="form-group">
-        {{ Form::submit('Edit event', array('class' => 'btn btn-primary')) }}
+        {{ Form::submit('Сохранить', array('class' => 'btn btn-primary')) }}
     </div>
 
 {{ Form::close() }}
